b2748f84cede27c603c88deaacb5041282489acf
d-ivashchuk <dimaiv@me.com>
2022-11-29 20:42:59 +0100

    use oss version

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   lostPixelPlatform: 'https://api.staging.lost-pixel.com',
-  lostPixelProjectId: 'cl9mjw1n41153201q9ulrxccgg',
-  apiKey: '5467a2d1-9245-49a4-8a98-f92b6f838822',
+  generateOnly: true,
+  failOnDifference: true,
+  // lostPixelProjectId: 'cl9mjw1n41153201q9ulrxccgg',
+  // apiKey: '5467a2d1-9245-49a4-8a98-f92b6f838822',
 
   storybookShots: {
     storybookUrl: 'storybook-static',
